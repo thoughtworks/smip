70bb85bee77b3574cab999385291763535394383
Atri Sarkar <atri.sarkar@thoughtworks.com>
2024-04-30 17:40:59 +0530

    [Atri/Gnana] Add abstraction layer prototype

--- smip_core/src/lib.rs
+++ smip_core/src/lib.rs
@@ -1,13 +1,36 @@
-pub trait Service {}
+use std::net::IpAddr;
 
-pub trait Message {}
-
-pub trait Payload {}
-
-pub struct Configuration {
-    
+pub struct Header {
 }
+pub type ServiceId = u16;
+
+pub struct ServiceConfig {
+    pub id: ServiceId, 
+    pub address: IpAddr,
+    pub port: u16,
+}
+
+pub trait Service {
+    type B: Backend;
+}
+
+pub trait Message {
+    type B: Backend;
+    fn header(&self) -> &Header;
+    fn payload(&self) -> &<Self::B as Backend>::P;
+}
+
+pub trait Payload {
+    type B: Backend;
+    fn with_data(data: &[u8]) -> Self where Self: Sized;
+    fn data(&self) -> &[u8];
+}
+
 pub trait Backend {
-    type M: Message
-    type P: Payload
+    type S: Service;
+    type M: Message;
+    type P: Payload;
+    fn create_service(config: &ServiceConfig) -> Self::S;
+    fn create_payload(data: &[u8]) -> Self::P;
+    fn create_message(header: &Header, payload: Self::P) -> Self::M;
 }
