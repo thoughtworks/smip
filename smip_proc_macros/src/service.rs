c545ab973270fd4aa590b2b6ffa8dafba4a874b8
Atri Sarkar <atri.sarkar@thoughtworks.com>
2024-05-03 13:05:55 +0000

    Use vsomeip types

--- smip_proc_macros/src/service.rs
+++ smip_proc_macros/src/service.rs
@@ -1,8 +1,9 @@
 use darling::FromMeta;
+use someip_types::{MajorVersion, MinorVersion};
 
 #[derive(Debug, FromMeta)]
 pub struct ServiceArgs {
     pub id: u16,
-    pub major_version: u8,
-    pub minor_version: u32
+    pub major_version: MajorVersion,
+    pub minor_version: MinorVersion
 }
