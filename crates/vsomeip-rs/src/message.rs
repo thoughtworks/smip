b633ba8434bf856d6c2890530e038eadcb44a6b9
Atri Sarkar <atri.sarkar@thoughtworks.com>
2024-05-06 10:35:22 +0000

    Add more getters/setters to Message

--- crates/vsomeip-rs/src/message.rs
+++ crates/vsomeip-rs/src/message.rs
@@ -1,6 +1,8 @@
 use cxx::*;
 
-use crate::{util::{self, AsPinMut}, InstanceId, MessageType, MethodId, Payload, ReturnCode, Runtime, ServiceId};
+use crate::*;
+use crate::util::AsPinMut;
+
 pub struct Message {
     pub(crate) inner: SharedPtr<vsomeip_sys::message>
 }
@@ -31,6 +33,18 @@ impl Message {
         let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
         unsafe { vsomeip_sys::message_base::get_instance(&message_base) }
     }
+    pub fn get_client(&self) -> ClientId {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        unsafe { vsomeip_sys::message_base::get_client(&message_base) }
+    }
+    pub fn get_session(&self) -> SessionId {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        unsafe { vsomeip_sys::message_base::get_session(&message_base) }
+    }
+    pub fn get_request(&self) -> RequestId {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        unsafe { vsomeip_sys::message_base::get_request(&message_base) }
+    }
     pub fn get_method(&self) -> MethodId {
         let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
         unsafe { vsomeip_sys::message_base::get_method(&message_base) }
@@ -39,6 +53,14 @@ impl Message {
         let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
         let return_code = unsafe { vsomeip_sys::message_base::get_return_code(&message_base) };
         return_code.into()
+    }
+    pub fn get_interface_version(&self) -> InterfaceVersion {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        unsafe { vsomeip_sys::message_base::get_interface_version(&message_base) }
+    }
+    pub fn get_protocol_version(&self) -> ProtocolVersion {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        unsafe { vsomeip_sys::message_base::get_protocol_version(&message_base) }
     }
     pub fn get_message_type(&self) -> MessageType {
         let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
@@ -52,7 +74,8 @@ impl Message {
             inner: payload
         }
     }
-    pub fn set_service(&mut self, service_id: u16) {
+
+    pub fn set_service(&mut self, service_id: ServiceId) {
         // FIXME: Converting to SharedPtr<message_base> is costly 
         // autocxx doesn't generate methods on the base class currently
         // Ideally we should have wrapper functions the methods on the base class message_base in C++
@@ -60,12 +83,32 @@ impl Message {
         let pin_mut = unsafe { util::shared_to_pin(&message_base) };
         unsafe { vsomeip_sys::message_base::set_service(pin_mut, service_id) };
     }
-    pub fn set_instance(&mut self, instance_id: u16) {
+    pub fn set_instance(&mut self, instance_id: InstanceId) {
         let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
         let pin_mut = unsafe { util::shared_to_pin(&message_base) };
         unsafe { vsomeip_sys::message_base::set_instance(pin_mut, instance_id) };
     }
-    pub fn set_method(&mut self, method_id: u16) {
+    pub fn set_client(&mut self, client_id: ClientId) {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        let pin_mut = unsafe { util::shared_to_pin(&message_base) };
+        unsafe { vsomeip_sys::message_base::set_client(pin_mut, client_id) };
+    }
+    pub fn set_session(&mut self, session_id: SessionId) {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        let pin_mut = unsafe { util::shared_to_pin(&message_base) };
+        unsafe { vsomeip_sys::message_base::set_session(pin_mut, session_id) };
+    }
+    pub fn set_interface_version(&mut self, interface_version: InterfaceVersion) {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        let pin_mut = unsafe { util::shared_to_pin(&message_base) };
+        unsafe { vsomeip_sys::message_base::set_interface_version(pin_mut, interface_version) };
+    }
+    pub fn set_message_type(&mut self, message_type: MessageType) {
+        let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
+        let pin_mut = unsafe { util::shared_to_pin(&message_base) };
+        unsafe { vsomeip_sys::message_base::set_message_type(pin_mut, message_type.into()) };
+    }
+    pub fn set_method(&mut self, method_id: MethodId) {
         let message_base = unsafe { vsomeip_sys::as_message_base(&self.inner) };
         let pin_mut = unsafe { util::shared_to_pin(&message_base) };
         unsafe { vsomeip_sys::message_base::set_method(pin_mut, method_id) };
@@ -94,6 +137,10 @@ mod tests {
 
         assert_eq!(message.get_service(), 0);
         assert_eq!(message.get_instance(), 0);
+        assert_eq!(message.get_client(), 0);
+        assert_eq!(message.get_session(), 0);
+        assert_eq!(message.get_interface_version(), 0);
+        assert_eq!(message.get_protocol_version(), 1);
         assert_eq!(message.get_method(), 0);
         assert_eq!(message.get_return_code(), ReturnCode::Ok);
         assert_eq!(message.get_message_type(), MessageType::Unknown);
@@ -104,6 +151,15 @@ mod tests {
 
         message.set_service(1234);
         assert_eq!(message.get_service(), 1234);
+
+        message.set_client(5678);
+        assert_eq!(message.get_client(), 5678);
+
+        message.set_session(9012);
+        assert_eq!(message.get_session(), 9012);
+
+        message.set_interface_version(123);
+        assert_eq!(message.get_interface_version(), 123);
 
         message.set_instance(4567);
         assert_eq!(message.get_instance(), 4567);
